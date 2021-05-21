a03b4016c41c61a5be88bfa32851ac6ca5f39349
Arsenio Soto Junior <sotosword@gmail.com>
2021-05-22 04:58:00 +1100

    Versao oficial do Aplicativo

---
+++ src/app/home/home.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { SignInComponent } from './signin/signin.component';
+import { FormsModule, ReactiveFormsModule }  from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { VMessageModule } from '../shared/components/vmessage/vmessage.module';
+import { RouterModule } from '@angular/router';
+import { SignUpComponent } from './signup/signup.component';
+
+@NgModule({
+    declarations: [
+         SignInComponent,
+         SignUpComponent
+    ],
+    imports: [ 
+        CommonModule,
+        FormsModule,
+        ReactiveFormsModule,
+        VMessageModule,
+        RouterModule
+    ]
+})
+export class HomeModule { }
---
+++ src/app/home/signup/signup.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { lowerCaseValidator } from 'src/app/shared/validators/lower-case.validator';
+import { UserNotTakenValidatorService } from './user-not-taken.validator.service';
+
+@Component({
+    templateUrl: './signup.component.html'
+})
+export class SignUpComponent implements OnInit {
+
+    signupForm: FormGroup;
+
+    constructor(
+        private formBuilder: FormBuilder,
+        private userNotTakenValidatorService: UserNotTakenValidatorService) {}
+
+    //validators ---> validando os campos
+    ngOnInit(): void {
+        
+        this.signupForm = this.formBuilder.group({
+            email: ['',
+                [
+                    Validators.required,
+                    Validators.email
+                ]
+            ],
+            fullName: ['',
+                [
+                    Validators.required,
+                    Validators.minLength(2),
+                    Validators.maxLength(40)
+                ]
+            ],
+            userName: ['',
+                [
+                    Validators.required,
+                    lowerCaseValidator, //comencando com letras mausculas
+                    Validators.minLength(2),
+                    Validators.maxLength(30)
+                ],
+               this.userNotTakenValidatorService.checkUserNameTaken()
+            ],
+            password: ['',
+                [
+                    Validators.required,
+                    Validators.minLength(8),
+                    Validators.maxLength(14)
+                ]
+            ]
+        });
+    }
+}
